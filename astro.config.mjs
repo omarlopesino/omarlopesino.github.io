d86630333e83ad7bea86d2ad89694e350ac690e7
Omar <omarmoper@gmail.com>
2026-06-24 00:34:58 +0200

    Discard custom script to compress in favour or build or CDN

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,11 @@ import react from '@astrojs/react';
 
 import mdx from '@astrojs/mdx';
 
-import { precompress } from './precompress.mjs';
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
       plugins: [tailwindcss()],
     },
 
-  integrations: [react(), mdx(), precompress()],
+  integrations: [react(), mdx()],
 });
