a67c482f013b6c972ca4528aab9a18e4271fea24
Omar <omarmoper@gmail.com>
2026-06-29 20:05:05 +0200

    Details component

---
+++ src/components/ui/Details.stories.ts
@@ -0,0 +1,10 @@
+import Details from "./Details.astro";
+export default {
+  title: 'Components/Details',
+  component: Details,
+  args:  {
+    title: "Details title",
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+};
+export const Default = {};
