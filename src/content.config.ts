9d47c974011dca0c6e7b91b99a6e9c2697919f67
Omar <omarmoper@gmail.com>
2026-07-01 21:51:31 +0200

    Define multilingual content collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,24 +1,56 @@
 import { defineCollection, reference } from 'astro:content';
 import { z } from 'astro/zod';
+import { file, glob } from 'astro/loaders';
+
+// @todo evaluate if we can calculate language from path!
+const taxonomyCollection =  (collectionName : string) => {
+	return defineCollection({
+		loader: glob({
+			base: "./src/data", 
+			pattern: collectionName + "/**/*.json",
+			generateId: ({data}) => {
+				// @todo use ID instead of slug.
+				return data.id + '/' + data.language;
+			},
+		}),
+		schema: z.object({
+			'name': z.string(),
+			'path': z.string(),
+			'language': z.string(),
+			'id': z.string(),
+		}),
+	});
+}
+
+const category = taxonomyCollection('categories');
+
+const tag = taxonomyCollection('tags');
 
 const blog = defineCollection({
-	type: 'content',
+  	loader: glob({ 
+		base: './src/blog', 
+		pattern: '**/*.{md,mdx}',
+		generateId: ({data}) => {
+			// @todo use ID instead of slug.
+			return data.slug + '/' + data.language;
+		},
+	}),
 	schema: z.object({
-		title: z.string(),
         slug: z.string(),
+		title: z.string(),
 		description: z.string(),
 		language: z.string(),
 		pubDate: z.coerce.date(),
 		image: z.object({
-            url: z.string(),
+            src: z.string(),
             alt: z.string(),
+			width: z.int().default(960),
+			height: z.int().default(540)
         }),
-        category: z.string(),
-		tags: z.array(z.string()) ,
-        recomme3nded: z.array(reference("content")),
+        category: reference('category'),
+		tags: z.array(reference('tag')) ,
+        recommended: z.optional(z.array(reference("blog"))),
 	}),
 });
 
-export const collections = {
-  'blog': blog
-};
+export const collections = { category, tag, blog };
