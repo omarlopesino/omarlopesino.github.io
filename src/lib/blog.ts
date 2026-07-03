441cbb61e8507bb3a70fe2735e85bac63bf1db48
Omar <omarmoper@gmail.com>
2026-07-04 00:44:55 +0200

    Show link alternates in post layout

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -1,5 +1,6 @@
 import type { GetStaticPaths } from "astro";
-import { getCollection, type CollectionKey } from "astro:content";
+import { getCollection, type CollectionKey, type DataEntryMap } from "astro:content";
+import type { Alternate } from "@/i18n/routes";
 
 export function staticPaths(type: CollectionKey, language: string) : GetStaticPaths {
   return async () => {
@@ -12,4 +13,23 @@ export function staticPaths(type: CollectionKey, language: string) : GetStaticPa
       props: { entry },
     }));
   };
+}
+
+export async function getContentAlternates(type : keyof DataEntryMap, entry : {language: string, cid: string}) {
+  return await Promise.all(
+    await getCollection(type, ({data}) => {
+      return data.cid == entry.cid
+  }));
+}
+
+export async function getContentAlternateUrls(type : keyof DataEntryMap, entry : {language: string, cid: string}) : Promise<Alternate[]> {
+  return (await getContentAlternates(type, entry))
+    .map(
+    (entry) => {
+      return {
+        'lang': entry?.data.language  || '',
+        'path': '/' + entry?.data.language + '/blog/' + entry?.data.slug,
+      };
+    }
+  );
 }
