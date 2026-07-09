f119f359a2429cd3457ba5ac089b2ac778a7076c
Omar <omarmoper@gmail.com>
2026-07-09 22:47:20 +0200

    Update routing

--- src/i18n/routes.ts
+++ src/i18n/routes.ts
@@ -1,6 +1,8 @@
 export const routes = {
   home: { en: '/en/', es: '/es/' },
-  blog: { en: '/en/blog', es: '/es/blog'}
+  blog: { en: '/en/blog', es: '/es/blog'},
+  blogTags: { en: '/en/blog/tags', es: '/es/blog/etiquetas'},
+  blogCategories: { en: '/en/blog/categories', es: '/es/blog/categorias'}
 } as const;
 
 export type RouteKey = keyof typeof routes;
